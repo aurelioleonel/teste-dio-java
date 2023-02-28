d441b2a910e20c9648c3e8723f03153b5655eae1
MarcosAurelio <aurelioleonel@gmail.com>
2023-02-27 22:16:12 -0300

    Correção na calculadora na operação divisão foi adicionado o double

--- src/com/dio/calculadora/Calculadora.java
+++ src/com/dio/calculadora/Calculadora.java
@@ -7,34 +7,34 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 				
-				double a, b;
+				int a, b;
 		System.out.println("Digite o primeiro valor");
 		a = scan.nextInt();
 		
 		System.out.println("Digite o segundo valor");
 		b = scan.nextInt();
 		
-		double soma = soma(a,b);
-		double subtracao = subtracao(a,b);
-		double multiplicacao = multiplicacao(a,b);
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int multiplicacao = multiplicacao(a,b);
 		double divisao = divisao(a,b);
 		
 		System.out.println("");
-		System.out.println(soma);
-		System.out.println(subtracao);
-		System.out.println(multiplicacao);
-		System.out.println(divisao);
+		System.out.println("Soma " + soma);
+		System.out.println("Subtracao " + subtracao);
+		System.out.println("Mult " + multiplicacao);
+		System.out.println("Div " + divisao);
 
 	}
 
-	public static double soma(double a, double b) {
+	public static int soma(int a, int b) {
 		return a + b;
 	};
 
-	public static double subtracao(double a, double b) {
+	public static int subtracao(int a, int b) {
 		return a - b;
 	};
-	public static double multiplicacao(double a, double b) {
+	public static int multiplicacao(int a, int b) {
 		return a * b;
 	};
 	public static double divisao(double a, double b) {
